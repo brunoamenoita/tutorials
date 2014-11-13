10b7417849e76cc9c6b319a218104a56c840afb3
Will Mitchell <will@onsip.com>
2014-11-13 17:58:43 -0500

    5 - Send your first Invite

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 /* This is the InstaCall constructor. */
 function InstaCall() {
+  this.remoteMedia = document.getElementById('remote-media');
 
+  this.inviteButton = document.getElementById('invite-button');
+  this.inviteButton.addEventListener('click', this.sendInvite.bind(this), false);
+
+  this.terminateButton = document.getElementById('terminate-button');
+  this.terminateButton.addEventListener('click', this.terminateSession.bind(this), false);
 }
 
 /* This is the InstaCall prototype. */
@@ -8,6 +14,46 @@ InstaCall.prototype = {
 
   createUA: function () {
     this.ua = new SIP.UA();
+  },
+
+  sendInvite: function () {
+    var session = this.ua.invite('welcome@onsip.com', this.remoteMedia);
+
+    this.setSession(session);
+    this.inviteButton.disabled = true;
+  },
+
+  setSession: function (session) {
+    session.on('progress', function () {
+      this.setStatus('Ringing...', true);
+    }.bind(this));
+
+    session.on('accepted', function () {
+      this.setStatus('Connected!', true);
+    }.bind(this));
+
+    session.on('failed', function () {
+      this.setStatus('Call failed. Try again?', false);
+      delete this.session;
+    }.bind(this));
+
+    session.on('bye', function () {
+      this.setStatus('Bye! Invite Another?', false);
+      delete this.session;
+    }.bind(this));
+
+    this.session = session;
+  },
+
+  setStatus: function (status, disable) {
+    this.inviteButton.innerHTML = status;
+    this.inviteButton.disabled = disable;
+  },
+
+  terminateSession: function () {
+      if (!this.session) { return; }
+
+      this.session.terminate();
   },
 
 };
